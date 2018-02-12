acde6f3367657ee24605ecd81455b1c41ae61038
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2018-02-11 23:39:57 -0800

    cleaner token-counts

--- source/Art.SuiteDemos/Demos/TicTacToe/TTT_react_comparison.jsx
+++ source/Art.SuiteDemos/Demos/TicTacToe/TTT_react_comparison.jsx
@@ -1,23 +1,23 @@
-body { // 10 // CSS tokens: 112  // Total tokens: 796
+body { // 10 token | 112 CSS tokens | 796 tokens in total
   font: 14px "Century Gothic", Futura, sans-serif;
   margin: 20px;
 }
 
-ol, ul { // 8
+ol, ul { // 8 tokens
   padding-left: 30px;
 }
 
-.board-row:after { // 14
+.board-row:after { // 14 tokens
   clear:    both;
   content:  "";
   display:  table;
 }
 
-.status { // 6
+.status { // 6 tokens
   margin-bottom: 10px;
 }
 
-.square { // 41
+.square { // 41 tokens
   background:   #fff;
   border:       1px solid #999;
   float:        left;
@@ -32,24 +32,24 @@ ol, ul { // 8
   width:        34px;
 }
 
-.square:focus { // 8
+.square:focus { // 8 tokens
   outline: none;
 }
 
-.kbd-navigation .square:focus { // 9
+.kbd-navigation .square:focus { // 9 tokens
   background: #ddd;
 }
 
-.game { // 9
+.game { // 9 tokens
   display: flex;
   flex-direction: row;
 }
 
-.game-info { // 7
+.game-info { // 7 tokens
   margin-left: 20px;
 }
 
-function Square(props) { // 33 // JSX tokens: 684
+function Square(props) { // 33 tokens | JSX tokens: 684
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
@@ -57,8 +57,8 @@ function Square(props) { // 33 // JSX tokens: 684
   );
 }
 
-class Board extends React.Component { // 166
-  renderSquare(i) { // 39
+class Board extends React.Component { // 166 tokens
+  renderSquare(i) {
     return (
       <Square
         value={this.props.squares[i]}
@@ -67,7 +67,7 @@ class Board extends React.Component { // 166
     );
   }
 
-  render() { // 119
+  render() { // 119 tokens
     return (
       <div>
         <div className="board-row">
@@ -90,8 +90,8 @@ class Board extends React.Component { // 166
   }
 } // 1
 
-class Game extends React.Component { // 7 => 372 total
-  constructor(props) { // 39
+class Game extends React.Component { // 372 tokens
+  constructor(props) {
     super(props);
     this.state = {
       history: [
@@ -104,7 +104,7 @@ class Game extends React.Component { // 7 => 372 total
     };
   }
 
-  handleClick(i) { // 117
+  handleClick(i) { // 117 tokens
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
@@ -123,14 +123,14 @@ class Game extends React.Component { // 7 => 372 total
     });
   }
 
-  jumpTo(step) { // 25
+  jumpTo(step) { // 25 tokens
     this.setState({
       stepNumber: step,
       xIsNext: (step % 2) === 0
     });
   }
 
-  render() { // 183
+  render() { // 183 tokens
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
@@ -170,10 +170,10 @@ class Game extends React.Component { // 7 => 372 total
   }
 }  // 1
 
-ReactDOM.render(<Game />, document.getElementById("root")); // 17
+ReactDOM.render(<Game />, document.getElementById("root")); // 17 tokens
 
-function calculateWinner(squares) { // 6 => 146
-  const lines = [ // 69
+function calculateWinner(squares) { // 146 tokens
+  const lines = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -183,7 +183,7 @@ function calculateWinner(squares) { // 6 => 146
     [0, 4, 8],
     [2, 4, 6]
   ];
-  for (let i = 0; i < lines.length; i++) { // 71
+  for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
